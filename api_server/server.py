6b43c35d821f4593d9311f0e56f2cf3d57f25691
Simon Brulhart <simon@brulhart.me>
2016-07-31 22:35:17 +0200

    Download videos through the API bridge

--- api_server/server.py
+++ api_server/server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import urllib
+from sys import stderr
+from subprocess import Popen, PIPE
 
 from bottle import Bottle, request, response
 from youtube_dl import YoutubeDL
@@ -10,7 +12,7 @@ app = Bottle()
 
 
 @app.route('/info/<url:path>')
-def v_dash_aac128(url):
+def v_info_url(url):
     url = urllib.parse.unquote(url)  # workaround
     format = request.forms.get('format', 'best')
     ytdl = YoutubeDL({'format': format, 'source_address': '0.0.0.0'})
@@ -21,8 +23,23 @@ def v_dash_aac128(url):
 
 
 @app.route('/info/<vid>')
-def v_dash_aac128_vid(vid):
-    v_dash_aac128("https://youtube.com/?v="+vid)
+def v_info_vid(vid):
+    v_info_url("https://youtube.com/?v="+vid)
+
+
+@app.route('/dl/<url:path>')
+def v_dl_url(url):
+    url = urllib.parse.unquote(url)  # workaround
+    args = ['python3', '-m', 'youtube_dl', '-f', 'best', '-o', '-', '--', url]
+    with Popen(args, bufsize=-1, stdout=PIPE, stderr=stderr) as ps:
+        response.content_type = "video/mp4"
+        for chunk in ps.stdout:
+            yield chunk
+
+
+@app.route('/dl/<vid>')
+def v_dl_vid(vid):
+    v_dl_url("https://youtube.com/?v="+vid)
 
 
 app.run(host='0.0.0.0', reloader=True, debug=True, server='cherrypy', port=3001)
--- src/Player.js
+++ src/Player.js
@@ -2,29 +2,17 @@ import React, {Component} from 'react'
 import {ResponsiveEmbed} from 'react-bootstrap'
 
 import {getVideoInfo} from './utils'
+import config from '../config.json'
 
 export default class Player extends Component {
-    state = {
-        videoHotlink: null
-    }
-
-    componentDidMount() {
-        const {videoId} = this.props;
-
-        getVideoInfo(videoId)
-        .then(data => this.setState({
-            videoHotlink: data.url
-        }));
-    }
-
     render() {
         const {videoId, loop, poster, onCanPlay, onEnded} = this.props;
-        const {videoHotlink} = this.state;
+        const videoHotlink = `${config.bridgeServerURL}/dl/${videoId}`;
 
         const video = <ResponsiveEmbed a16by9>
             <video autoPlay controls loop={loop} poster={poster}
                     onCanPlay={onCanPlay} onEnded={onEnded}>
-                {videoHotlink && <source src={videoHotlink}/>}
+                <source src={videoHotlink}/>
             </video>
         </ResponsiveEmbed>;
 
